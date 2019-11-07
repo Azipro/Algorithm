079ad1161ab285fd71b4ee646d2bba066666bf8b
Azipro <48899069+Azipro@users.noreply.github.com>
2019-11-07 13:29:43 +0800

    Create Problem of Precision.cpp

---
+++ 矩阵快速幂/Problem of Precision.cpp
@@ -0,0 +1,24 @@
+//HDU 2256
+Problem Description
+
+
+
+Input
+The first line of input gives the number of cases, T. T test cases follow, each on a separate line. Each test case contains one positive integer n. (1 <= n <= 10^9)
+ 
+
+Output
+For each input case, you should output the answer in one line.
+ 
+
+Sample Input
+3
+1
+2
+5
+ 
+
+Sample Output
+9
+97
+841
